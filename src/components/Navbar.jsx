1a511c67377cce96c47f9c9f465d8b98287740c3
Atharv Mohite <154729565+atharv96k@users.noreply.github.com>
2025-10-26 11:38:15 +0530

    Wrap text in span for Navbar link

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -14,8 +14,8 @@ export default function Navbar() {
           to="/detection"  
           className="bg-blue-500 hover:bg-blue-600 text-white px-4 py-2 rounded-lg font-medium transition-colors" 
         > 
-         <img src={sparkIcon} alt="Spark icon" className="w-5 h-5" />
-          Fact Check Now 
+        <img src={sparkIcon} alt="Spark icon" className="w-5 h-5" />
+            <span>Fact Check Now</span>
         </Link> 
       </div> 
     </nav> 
